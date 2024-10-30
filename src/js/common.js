ba35b41c330c1e555bf1a8c3b1ab7371b5d01f2e
Nathan <antnefedov33@gmail.com>
2024-10-30 16:19:41 +0300

    header, nav menu, hamburger-menu, media for header

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const hamburger = document.querySelector('.hamburger');
+const header_menu_wrapper = document.querySelector('.header_menu-wrapper');
+const header_menu = document.querySelector('.header_menu');
+const header_menu_links = document.querySelectorAll('.header_menu li a');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	header_menu_wrapper.classList.toggle('active');
+});
+
+header_menu_links.forEach(link => {
+	link.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		header_menu_wrapper.classList.toggle('active');
+	});
+});
