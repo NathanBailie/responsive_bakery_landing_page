541fdec2662d6730032c5814e642de1a1f0c15ef
Nathan <antnefedov33@gmail.com>
2024-11-01 16:11:24 +0300

    scroll reveal animations

--- src/js/common.js
+++ src/js/common.js
@@ -62,3 +62,67 @@ window.addEventListener('scroll', () => {
 		? scrollUp.classList.add('active')
 		: scrollUp.classList.remove('active');
 });
+
+// animations
+const sr = ScrollReveal({
+	distance: '50px',
+	duration: 2500,
+	delay: 300,
+});
+
+// titles
+sr.reveal(`.news_title`, { delay: 600, origin: 'bottom' });
+sr.reveal(
+	`.about_title, .favorites_title, .visit_title, .location_logo, .location_title`,
+	{
+		delay: 600,
+		origin: 'top',
+	}
+);
+
+// descriptions, buttons
+sr.reveal(`.home_data`, { delay: 700, origin: 'left' });
+sr.reveal(`.visit_text, .location_descr`, { delay: 700, origin: 'bottom' });
+sr.reveal(`.favorites_item, .location_list-elem`, {
+	delay: 700,
+	origin: 'bottom',
+	interval: 150,
+});
+sr.reveal(`.home_contacts, .news_button-wrapper, .visit_button-wrapper`, {
+	delay: 900,
+	origin: 'bottom',
+});
+
+sr.reveal(`.about_button-wrapper`, {
+	delay: 900,
+});
+
+sr.reveal(`.about_descr`, {
+	delay: 700,
+	origin: 'bottom',
+});
+
+// images, lists
+sr.reveal(`.home_img-main`, {
+	delay: 900,
+	rotate: { z: -15 },
+	scale: 1.1,
+});
+sr.reveal(`.home_img-small, .location_img1, .location_img2`, {
+	delay: 900,
+	rotate: { z: 15 },
+});
+sr.reveal(`.home_link, .news_item, .location_link`, {
+	delay: 900,
+	origin: 'bottom',
+	interval: 150,
+});
+sr.reveal(`.about_image`, {
+	delay: 900,
+	origin: 'left',
+	scale: 1.1,
+});
+
+sr.reveal(`.about_bread2-img`, {
+	delay: 900,
+});
